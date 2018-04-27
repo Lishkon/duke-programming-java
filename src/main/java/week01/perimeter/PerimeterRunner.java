696b1d3a9af57e4157b74bb1637f10f998d98fb6
Aleksey Lychak <mybox-aleks@inbox.ru>
2018-04-27 17:45:21 +0300

    javadoc added

--- src/main/java/week01/perimeter/PerimeterRunner.java
+++ src/main/java/week01/perimeter/PerimeterRunner.java
@@ -2,6 +2,12 @@ package week01.perimeter;
 
 import edu.duke.*;
 
+/**
+ * @author alychak
+ * @url http://www.dukelearntoprogram.com/course2/doc/
+ * @url https://www.coursera.org/learn/java-programming/lecture/cI1Vw/seven-steps-in-action-translating-to-code
+ * */
+
 public class PerimeterRunner {
     public double getPerimeter (Shape s) {
         double totalPerim = 0;
