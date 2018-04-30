cc846e1caf343ac4f26d60fe422ed344b1bf1632
Aleksey Lychak <mybox-aleks@inbox.ru>
2018-04-30 15:35:36 +0300

    Few testes added

--- src/test/java/week01/perimeter/PerimeterRunnerTest.java
+++ src/test/java/week01/perimeter/PerimeterRunnerTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 public class PerimeterRunnerTest {
 
     @Test
-    public void testPerimeter () {
+    public void testPerimeterOne () {
         PerimeterRunner runner = new PerimeterRunner();
         Shape shape = new Shape();
         shape.addPoint(new Point(-1,3));
@@ -19,9 +19,33 @@ public class PerimeterRunnerTest {
 
         assertEquals(16.0, runner.getPerimeter(shape),0);
 
+    }
 
+    @Test
+    public void testPerimeterTwo () {
+        PerimeterRunner runner = new PerimeterRunner();
+        Shape shape = new Shape();
+        shape.addPoint(new Point(4,3));
+        shape.addPoint(new Point(2,-3));
+        shape.addPoint(new Point(-4,-4));
+        shape.addPoint(new Point(-2,-3));
+        shape.addPoint(new Point(-4,2));
+        shape.addPoint(new Point(-2,5));
 
-
+        assertEquals(30.0, runner.getPerimeter(shape),0.5);
 
     }
+
+    @Test
+    public void testPerimeterThree () {
+        PerimeterRunner runner = new PerimeterRunner();
+        Shape shape = new Shape();
+        shape.addPoint(new Point(-4,-6));
+        shape.addPoint(new Point(-4,-1));
+        shape.addPoint(new Point(-4,-3));
+
+        assertEquals(10.0, runner.getPerimeter(shape),0.5);
+
+    }
+
 }
